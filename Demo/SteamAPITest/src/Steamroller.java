d0109f2cb7fe2c28269f381e148c7958b3567c28
Nikolaos Bukas <nikolaosbukas@outlook.com>
2015-01-13 11:43:13 -0500

    Commit

--- Demo/SteamAPITest/src/Steamroller.java
+++
@@ -1,9 +0,0 @@
-import com.github.koraktor.steamcondenser.exceptions.WebApiException;
-import com.github.koraktor.steamcondenser.steam.community.WebApi;
-
-
-public class Steamroller extends WebApi {
-	public Steamroller(String apiKey) throws WebApiException {
-		this.setApiKey(apiKey);
-	}
-}
