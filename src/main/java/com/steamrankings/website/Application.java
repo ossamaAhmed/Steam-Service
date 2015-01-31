28c88a936102c1324ecd915df2fb44c887014704
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2015-01-31 01:36:47 -0500

    CSS/js now works

--- src/main/java/com/steamrankings/website/Application.java
+++ src/main/java/com/steamrankings/website/Application.java
@@ -1,15 +1,24 @@
 package com.steamrankings.website;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+@EnableWebMvc
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/assets/**")
+        .addResourceLocations("classpath:/assets/");
+    }
 }
--- src/main/java/com/steamrankings/website/controllers/IndexController.java
+++ src/main/java/com/steamrankings/website/controllers/IndexController.java
@@ -3,18 +3,21 @@ package com.steamrankings.website.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView model = new ModelAndView("index");
+		return model;
+
+	}
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String redirect() {
-      
+    public String redirect() {     
        return "redirect:/";
     }
 }
