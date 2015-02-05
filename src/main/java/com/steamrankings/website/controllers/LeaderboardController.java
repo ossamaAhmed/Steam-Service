ce6e4c0f8f078119c24d29a60771137df0bf2840
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2015-02-05 00:00:16 -0500

    dynamic table add works

--- src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
+++ src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.steamrankings.service.api.leaderboards.Leaderboards;
@@ -13,17 +14,12 @@ import com.steamrankings.service.api.leaderboards.RankEntryByAchievements;
 @Controller
 public class LeaderboardController {
 
-    @RequestMapping("/leaderboard")
-    public String getLeaderboard(Model model) {
-//        model.addAttribute("rank1", "32");
-//        model.addAttribute("name", "steve");
-//        model.addAttribute("achivements", "33");
-//        model.addAttribute("time", "453");
-//        model.addAttribute("country", "Canada");
-        
-        RankEntryByAchievements r1 = new RankEntryByAchievements("eric", new Long(1234), "Canada", 500, 50);
+    @ModelAttribute("rankentries")
+    public List<RankEntryByAchievements> populateTable() {
+        RankEntryByAchievements r1 = new RankEntryByAchievements("swag", new Long(1234), "Canada", 500, 50);
         RankEntryByAchievements r2 = new RankEntryByAchievements("yolo", new Long(4321), "USA", 501, 51);
-
+        r1.setRankNumber(1);
+        r2.setRankNumber(2);
         List<RankEntryByAchievements> curList = new ArrayList<RankEntryByAchievements>();
         curList.add(r1);
         curList.add(r2);
@@ -31,15 +27,11 @@ public class LeaderboardController {
         Leaderboards l = new Leaderboards(curList);
         
         List<RankEntryByAchievements> newList = l.getRanksByAchievementTotal(1, 2);
-        
-        for (int i = 0; i < newList.size(); i++) {
-            model.addAttribute("rank"+(i+1), newList.get(i).getRankNumber());
-            model.addAttribute("name"+(i+1), newList.get(i).getName());
-            model.addAttribute("achievement"+(i+1), newList.get(i).getTotalNumberOfAchievements());
-            model.addAttribute("time"+(i+1), newList.get(i).getAverageCompetionRate());
-            model.addAttribute("country"+(i+1), newList.get(i).getCountry());
-        }
-        
+        return newList;
+    }
+
+    @RequestMapping("/leaderboard")
+    public String getLeaderboard(Model model) {
     	return "leaderboard";
     }
 }
--- src/main/resources/assets/js/leaderboard.js
+++ src/main/resources/assets/js/leaderboard.js
@@ -1,24 +1,8 @@
 function addRow() {
 	$("#leaderboard-table tbody").append(
-		"<tr>" + "<th th:text='${rank2}'/>" +
-		"<td th:text='${name2}' />" +
-		"<td th:text='${achievement2}' />" +
-		"<td th:text='${time2}' />" +
-		"<td th:text='${country2}' />");
+		"<tr>" + "<th th:text=\"${rank2}\"/>" +
+		"<td th:text=\"${name2}\" />" +
+		"<td th:text=\"${achievement2}\" />" +
+		"<td th:text=\"${completion}\" />" +
+		"<td th:text=\"${country2}\" />") + "</tr>";
 };
-
-// function save() {
-// 	var par = $(this).parent().parent();
-
-// 	var trRank = par.children("tr:nth-child(0)");
-// 	var tdName = par.children("td:nth-child(1)");
-//  	var tdAchievements = par.children("td:nth-child(2)"); 
-//  	var tdTimePlayed = par.children("td:nth-child(3)"); 
-//  	var tdCountry = par.children("td:nth-child(4)"); 
-//  	tdName.html(tdName.children("input[type=text]").val());
-//   	tdAchievements.html(tdAchievements.children("input[type=text]").val()); 
-//   	tdTimePlayed.html(tdTimePlayed.children("input[type=text]").val());
-//   	tdTimePlayed.html(tdTimePlayed.children("input[type=text]").val());  	
-//   	tdButtons.html("<img src='images/delete.png' class='btnDelete'/><img src='images/pencil.png' class='btnEdit'/>");
-//    $(".btnEdit").bind("click", Edit); $(".btnDelete").bind("click", Delete); }; 
-// }
