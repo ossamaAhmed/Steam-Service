9feefaa8d3a14a57f215841ce89bee9dec8a8a93
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2015-02-06 15:06:17 -0500

    modified classpath - leaderboard broken

--- src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
+++ src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
@@ -1,6 +1,5 @@
 package com.steamrankings.website.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.steamrankings.service.api.leaderboards.Leaderboards;
 import com.steamrankings.service.api.leaderboards.RankEntryByAchievements;
 
 @Controller
@@ -16,18 +14,16 @@ public class LeaderboardController {
 
     @ModelAttribute("rankentries")
     public List<RankEntryByAchievements> populateTable() {
-        RankEntryByAchievements r1 = new RankEntryByAchievements("swag", new Long(1234), "Canada", 500, 50);
-        RankEntryByAchievements r2 = new RankEntryByAchievements("yolo", new Long(4321), "USA", 501, 51);
-        r1.setRankNumber(1);
-        r2.setRankNumber(2);
+        /*
+        RankEntryByAchievements r1 = new RankEntryByAchievements(1, 1234, "swag", 500, "50" , "Canada");
+        RankEntryByAchievements r2 = new RankEntryByAchievements(2, 4321, "yolo", 501, "51", "USA");
         List<RankEntryByAchievements> curList = new ArrayList<RankEntryByAchievements>();
         curList.add(r1);
         curList.add(r2);
-        
-        Leaderboards l = new Leaderboards(curList);
-        
-        List<RankEntryByAchievements> newList = l.getRanksByAchievementTotal(1, 2);
-        return newList;
+
+        return curList;
+        */
+        return null;
     }
 
     @RequestMapping("/leaderboard")
