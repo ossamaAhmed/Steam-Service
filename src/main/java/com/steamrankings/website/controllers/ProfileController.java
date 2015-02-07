d9960157701169b9e8761b99ee410c4320012ffe
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-07 14:31:42 -0500

    Removed demo code from website

--- src/main/java/com/steamrankings/website/controllers/ProfileController.java
+++ src/main/java/com/steamrankings/website/controllers/ProfileController.java
@@ -1,18 +1,11 @@
 package com.steamrankings.website.controllers;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.steamrankings.service.api.profiles.Profiles;
 import com.steamrankings.service.api.profiles.SteamProfile;
-import com.steamrankings.website.SteamrollerApi;
 
 // HTTP requests handled by the controller
 // You identify controllers in Spring by using the annotation @Controller
@@ -31,58 +24,5 @@ public class ProfileController {
         model.addAttribute("message", profile.getSteamCommunityId());
         model.addAttribute("url", profile.getSteamCommunityUrl());
         return "profile";
-    }
-
-    public Map<String, String> getSteamNameFromId(String id) throws Exception {
-        // Replace XXXXX with the Steam API key found on OneDrive in ECSE 428
-        // Project\API examples\apikey.txt
-        // PLEASE DO NOT PUSH TO GITHUB WITH API KEY IN CODE
-        SteamrollerApi api = new SteamrollerApi("XXXXX");
-        Map<String, Object> param = new HashMap<String, Object>();
-        System.out.println(id);
-
-        // Specify our steam ID parameter
-        param.put("steamids", id);
-
-        // Calling the Steam API through steam-condenser library
-        // We specify the following:
-        // The interface ISteamUser
-        // The API method GetPlaterSummaries
-        // The version number 2
-        // And our paramaters
-        // All this info can be found at
-        // https://developer.valvesoftware.com/wiki/Steam_Web_API
-        String jsonString = api.getJSON("ISteamUser", "GetPlayerSummaries", 2, param);
-        System.out.println(jsonString);
-
-        // The JSON object returned by Steam
-        JSONObject json = new JSONObject(jsonString);
-        System.out.println(json);
-
-        // Now we just search for the variable "personaname" that contains the
-        // value we are looking for
-
-        json = (JSONObject) json.get("response");
-        System.out.println(json);
-
-        JSONArray jsonArray = json.getJSONArray("players");
-        System.out.println(jsonArray);
-
-        Map<String, String> map = new HashMap<String, String>();
-
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject jsonElement = jsonArray.getJSONObject(i);
-            Iterator itr = jsonElement.keys();
-            while (itr.hasNext()) {
-                String element = (String) itr.next();
-                if (element.equals("personaname")) {
-                    map.put("personaname", jsonElement.getString("personaname"));
-                } else if (element.equals("profileurl")) {
-                    map.put("profileurl", jsonElement.getString("profileurl"));
-                }
-            }
-        }
-
-        return map;
     }
 }
