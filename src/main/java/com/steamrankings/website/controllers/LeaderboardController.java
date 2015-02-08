fc3119ae44aecada0228e13c3ff000fe252b94eb
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-08 04:50:12 -0500

    Leaderboard controller now working

--- src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
+++ src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
@@ -8,27 +8,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.steamrankings.service.api.leaderboards.Leaderboards;
 import com.steamrankings.service.api.leaderboards.RankEntryByAchievements;
 
 @Controller
 public class LeaderboardController {
 
+    /*
     @ModelAttribute("rankentries")
     public List<RankEntryByAchievements> populateTable() {
-        
-        RankEntryByAchievements r1 = new RankEntryByAchievements(1, 1234, "swag", 500, "50" , "Canada");
+
+        RankEntryByAchievements r1 = new RankEntryByAchievements(1, 1234, "swag", 500, "50", "Canada");
         RankEntryByAchievements r2 = new RankEntryByAchievements(2, 4321, "yolo", 501, "51", "USA");
         List<RankEntryByAchievements> curList = new ArrayList<RankEntryByAchievements>();
         curList.add(r1);
         curList.add(r2);
 
         return curList;
-        
-//        return null;
     }
+    */
 
     @RequestMapping("/leaderboard")
     public String getLeaderboard(Model model) {
-    	return "leaderboard";
+        List<RankEntryByAchievements> rankEntries = Leaderboards.getRanksByAchievementTotal(1, 10);
+        model.addAttribute("rankentries", rankEntries);
+        return "leaderboard";
     }
 }
