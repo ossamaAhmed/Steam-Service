d84ea2417d8f23f46e14be38bdf6401ece6f8b47
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-08 04:13:15 -0500

    ProfileController now displays games and achievements

--- src/main/java/com/steamrankings/website/controllers/ProfileController.java
+++ src/main/java/com/steamrankings/website/controllers/ProfileController.java
@@ -1,16 +1,15 @@
 package com.steamrankings.website.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.steamrankings.service.api.achievements.Achievements;
+import com.steamrankings.service.api.achievements.GameAchievement;
 import com.steamrankings.service.api.games.Games;
 import com.steamrankings.service.api.games.SteamGame;
-import com.steamrankings.service.api.leaderboards.RankEntryByAchievements;
 import com.steamrankings.service.api.profiles.Profiles;
 import com.steamrankings.service.api.profiles.SteamProfile;
 
@@ -30,37 +29,14 @@ public class ProfileController {
         SteamProfile profile = Profiles.getSteamUser(id);
         model.addAttribute("full_avatar_url", profile.getFullAvatarUrl());
         model.addAttribute("personal_name", profile.getPersonaName());
-        model.addAttribute("message", profile.getSteamCommunityId());
         model.addAttribute("url", profile.getSteamCommunityUrl());
+
+        List<SteamGame> games = Games.getPlayedSteamGames(id);
+        model.addAttribute("games", games);
+
+        List<GameAchievement> achievements = Achievements.getUnlockedAchievements(id);
+        model.addAttribute("achievements", achievements);
 
         return "profile";
     }
-
-//	@ModelAttribute("ID")
-//	public String getID(){
-//		SteamProfile profile = Profiles.getSteamUser(id);
-//	}
-	
-    @ModelAttribute("games")
-    public List<SteamGame> populateGames() {
-        SteamGame gameOne = new SteamGame(123, "icon", "http://logonoid.com/images/thumbs/cs-logo.png", "Counter Strike");
-        SteamGame gameTwo = new SteamGame(1234, "icon2", "logourl2", "Counter Strike Source");
-
-        List<SteamGame> curList = new ArrayList<SteamGame>();
-        curList.add(gameOne);
-        curList.add(gameTwo);
-
-        return curList;
-        
-//        return null;
-    }
-
-//    @ModelAttribute("games")
-//    public List<SteamGame> populateGames(String id) {     
-//        List<SteamGame> games = Games.getPlayedSteamGames(id);
-//        if (games.isEmpty()) {
-//        	System.out.println("empty");
-//        }
-//        return games;
-//    }
 }
