92be23cd0ea3263304c0fb0d1a788ee9a05443af
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2015-02-09 18:25:24 -0500

    added url query

--- src/main/java/com/steamrankings/website/controllers/IndexController.java
+++ src/main/java/com/steamrankings/website/controllers/IndexController.java
@@ -12,26 +12,39 @@ import com.steamrankings.website.model.SteamProfile;
 @Controller
 public class IndexController extends WebMvcConfigurerAdapter {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String showIndex(SteamProfile profile, String error, Model model) {
-    	if (error != null) {
-    		model.addAttribute("error", error);
-    	}
-        return "index";
-    }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showIndex(SteamProfile profile, String error, Model model) {
+		if (error != null) {
+			model.addAttribute("error", error);
+		}
+		return "index";
+	}
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String redirect(SteamProfile profile) {
-        return "redirect:/";
-    }
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String redirect(SteamProfile profile) {
+		return "redirect:/";
+	}
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String checkPersonInfo(SteamProfile steamProfile, BindingResult bindingResult) {
-       if (bindingResult.hasErrors()) {
-           return "index";
-       }
-       
-       return "redirect:/profile?id=" + steamProfile.getSteamId64();
-    }
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String checkPersonInfo(SteamProfile steamProfile,
+			BindingResult bindingResult) {
 
+		if (bindingResult.hasErrors()) {
+			return "index";
+		}
+
+		return "redirect:/profile?id="
+				+ handleQuery(steamProfile.getUserQuery());
+	}
+
+	/* Parse url otherwise, return text */
+	private String handleQuery(String query) {
+		String match = "steamcommunity.com/id/";
+		int index = query.indexOf(match);
+		if (index != -1) {
+			index += match.length();
+			return query.substring(index, query.length());
+		}
+		return query;
+	}
 }
--- src/main/java/com/steamrankings/website/model/SteamProfile.java
+++ src/main/java/com/steamrankings/website/model/SteamProfile.java
@@ -3,14 +3,14 @@ package com.steamrankings.website.model;
 import javax.validation.constraints.NotNull;
 
 public class SteamProfile {
-    private String id64;
+    private String id64OrName;
 
     @NotNull
-    public String getSteamId64() {
-        return id64;
+    public String getUserQuery() {
+        return id64OrName;
     }
 
-    public void setSteamId64(String query) {
-        this.id64 = query;
+    public void setUserQuery(String query) {
+        this.id64OrName = query;
     }
 }
