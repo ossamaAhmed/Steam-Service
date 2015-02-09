2109b4ab4a2a789a743a8dfa48455f9feb457ba5
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-08 20:19:39 -0500

    Fixed the model

--- src/main/java/com/steamrankings/website/model/SteamProfile.java
+++ src/main/java/com/steamrankings/website/model/SteamProfile.java
@@ -2,29 +2,15 @@ package com.steamrankings.website.model;
 
 import javax.validation.constraints.NotNull;
 
-import com.steamrankings.service.steam.SteamDataDatabase;
-
 public class SteamProfile {
-    private Long id64;
-
+    private String id64;
 
     @NotNull
-    public Long getSteamId64() {
+    public String getSteamId64() {
         return id64;
     }
 
     public void setSteamId64(String query) {
-        this.id64 = SteamDataDatabase.convertToSteamId64(query);
+        this.id64 = query;
     }
-
-//    public void setSteamId64(String query) {
-//        this.id64 = query;
-//    }
-//    private String detectIdOrName(String query) {
-//    	if (SteamDataDatabase.convertToSteamId64(id64) == -1) {
-//    		return query;
-//    	} else {
-//    		return id
-//    	}
-//    }
 }
