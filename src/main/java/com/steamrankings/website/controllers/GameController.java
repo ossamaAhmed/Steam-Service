607580c665ab78e42eb23642ae1572afac536677
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-27 18:29:13 -0500

    tests

--- src/main/java/com/steamrankings/website/controllers/GameController.java
+++ src/main/java/com/steamrankings/website/controllers/GameController.java
@@ -1,5 +1,9 @@
 package com.steamrankings.website.controllers;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,30 +12,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.steamrankings.service.api.APIException;
 import com.steamrankings.service.api.games.Games;
 import com.steamrankings.service.api.games.SteamGame;
+import com.steamrankings.website.Application;
 
 @Controller
 public class GameController {
     @RequestMapping("/game")
     public String getGame(String id, Model model) throws JSONException {
-        // We add the error message to our model
-    	
-    	SteamGame game = null;
-//    	try {
-    		game = Games.getSteamGame(500);
-//    	}
-//        catch (Exception e) {
-//        	if (e instanceof APIException) {
-//        		return "redirect:/?error=" + e.getMessage();
-//        	}
-//        }
-    	
-//        if (game == null) {
-//            return "index";
-//        }
 
-//        model.addAttribute("logoUrl", game.getLogoUrl());
-        model.addAttribute("name", game.getName());        
-//        model.ad
+        SteamGame game = null;
+
+        try {
+            game = Games.getSteamGame(500, Application.client);
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (APIException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        model.addAttribute("name", game.getName());
+
         return "game";
     }
 }
