b81d8e3857b1103185cda139c4e5b8c9f199c2a2
sryan28 <sean.ryan3@mail.mcgill.ca>
2015-02-27 11:51:04 -0500

    commit

--- src/main/java/com/steamrankings/website/controllers/GamesController.java
+++ src/main/java/com/steamrankings/website/controllers/GamesController.java
@@ -14,9 +14,19 @@ public class GamesController {
 	public String getGamesLeaderboard(Model model) {
 		List<RankEntryByTotalPlayTime> rankEntries = Games.getRanksByTotalPlayTime(1, 10);
 		for(int i = 0; i < rankEntries.size(); i++) {
-			if(rankEntries.get(i).)
+			if(rankEntries.get(i).getCountryCode() != null && !rankEntries.get(i).getCountryCode().equals("")) {
+				  String countryFlag = "/assets/images/country_flags/" + rankEntries.get(i).getCountryCode().toLowerCase() + ".png";
+				  rankEntries.set(i, new RankEntryByTotalPlayTime(rankEntries.get(i).getRankNumber(), rankEntries.get(i).getId64(),
+						  rankEntries.get(i).getName(),rankEntries.get(i).getTotalPlayTime(), countryFlag));
+			} else {
+                String countryFlag = "/assets/images/country_flags/_United Nations.png";
+                rankEntries.set(i, new RankEntryByTotalPlayTime(rankEntries.get(i).getRankNumber(), rankEntries.get(i).getId64(),
+						  rankEntries.get(i).getName(),rankEntries.get(i).getTotalPlayTime(), countryFlag));
+			}
 		}
+		model.addAttribute("rankEntries", rankEntries);
+		return "games";
 	}
-
+	
+	//@RequestMapping("/games")
 }
-
