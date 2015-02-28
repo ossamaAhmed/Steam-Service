18752ea32718f1d7b0afdae7cbd261e294e78c76
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-28 14:28:03 -0500

    Added links in the countries page. Uploaded new flags

--- src/main/java/com/steamrankings/website/controllers/CountriesController.java
+++ src/main/java/com/steamrankings/website/controllers/CountriesController.java
@@ -15,35 +15,33 @@ public class CountriesController {
 
     @RequestMapping("/countries")
     public String getCountries(String id, Model model) {
-        // We add the error message to our model
-        ArrayList<Country> countries = new ArrayList<Country>();
+        if (id == null || id.isEmpty() || id.equals("")) {
+            ArrayList<Country> countries = new ArrayList<Country>();
 
-        for (String key : Application.steam_countries.keySet()) {
-            URL fileURL = this.getClass().getResource("/assets/images/country_flags/" + key + ".png");
-            File f;
-            try {
-                f = new File(fileURL.toURI());
-                if (f.isFile())
-                    countries.add(new Country(key, Application.steam_countries.getJSONObject(key).getString("name")));
-                else
-                    countries.add(new Country("_United Nations", Application.steam_countries.getJSONObject(key).getString("name")));
-            } catch (Exception e) {
-                countries.add(new Country("_United Nations", Application.steam_countries.getJSONObject(key).getString("name")));
+            for (String key : Application.steam_countries.keySet()) {
+                URL fileURL = this.getClass().getResource("/assets/images/country_flags/64/" + key + ".png");
+                File f;
+                try {
+                    f = new File(fileURL.toURI());
+                    if (f.isFile())
+                        countries.add(new Country(key, Application.steam_countries.getJSONObject(key).getString("name")));
+                    else
+                        countries.add(new Country("_United-Nations", Application.steam_countries.getJSONObject(key).getString("name")));
+                } catch (Exception e) {
+                    countries.add(new Country("_United-Nations", Application.steam_countries.getJSONObject(key).getString("name")));
+                }
+            }
+            model.addAttribute("countries", countries);
+            return "countries";
+        } else {
+            if (Application.steam_countries.has(id)) {
+                model.addAttribute("country_code", id);
+                model.addAttribute("country_name", Application.steam_countries.getJSONObject(id).getString("name"));
+                return "country";
+            } else {
+                model.addAttribute("error_msg", "The country code " + id + "is invalid.");
+                return "error";
             }
         }
-
-        if (id != null) {
-            return "redirect:/country?=" + id;
-        } else {
-            model.addAttribute("countries", countries);
-        }
-
-        return "countries";
-    }
-
-    @RequestMapping("/country")
-    public String getSpecificCountry(String id, Model model) {
-        model.addAttribute("country", "yo");
-        return "country";
     }
 }
