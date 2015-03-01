ba6ca416a6a4b4470f8a1a3f46e49bde6f1688ef
Michael Smith <michael.smith6@mail.mcgill.ca>
2015-02-28 19:20:59 -0500

    Fixed flags

--- src/main/java/com/steamrankings/website/controllers/CountriesController.java
+++ src/main/java/com/steamrankings/website/controllers/CountriesController.java
@@ -2,6 +2,7 @@ package com.steamrankings.website.controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,12 +26,11 @@ public class CountriesController {
             ArrayList<Country> countries = new ArrayList<Country>();
 
             for (String key : Application.steam_countries.keySet()) {
-                URL fileURL = this.getClass().getResource("/assets/images/country_flags/64/" + key + ".png");
-                File f;
+            	InputStream inStream = this.getClass().getResourceAsStream("/assets/images/country_flags/64/" + key.toLowerCase() + ".png");
                 try {
-                    f = new File(fileURL.toURI());
-                    if (f.isFile())
-                        countries.add(new Country(key, Application.steam_countries.getJSONObject(key).getString("name")));
+
+                    if (inStream != null)
+                        countries.add(new Country(key.toLowerCase(), Application.steam_countries.getJSONObject(key).getString("name")));
                     else
                         countries.add(new Country("_United-Nations", Application.steam_countries.getJSONObject(key).getString("name")));
                 } catch (Exception e) {
